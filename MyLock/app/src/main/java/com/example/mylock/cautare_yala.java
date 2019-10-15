969f8a39fd29fb80d6b81d8b4e6e5571dc96a6a3
CristianHilohi <48750997+CristianHilohi@users.noreply.github.com>
2019-10-16 02:52:22 +0300

    Add files via upload
    Am uploadat așa că nu mai am energie să văd cum ar trebui să fac. Ideea e că am făcut un proiect nou fiindcă nu găseam fișierele de care aveam nevoie pe git; vedem cum asamblăm ce am făcut.

---
+++ MyLock/app/src/main/java/com/example/mylock/cautare_yala.java
@@ -0,0 +1,14 @@
+package com.example.mylock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cautare_yala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cautare_yala);
+    }
+}
