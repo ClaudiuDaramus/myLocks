8ef9462980f82e1d859db148abc6353db4b38758
teodor440 <teo_paul1@yahoo.com>
2019-10-19 12:26:41 +0300

    Help claudiu

--- Android/app/src/main/java/com/example/mylocks/BluetoothService.java
+++ Android/app/src/main/java/com/example/mylocks/BluetoothService.java
@@ -2,13 +2,36 @@ package com.example.mylocks;
 // Poate duce un singur task la o anumita perioada (cautare continua)
 
 import android.app.IntentService;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 import androidx.annotation.Nullable;
+
+import java.util.Map;
+
+import me.aflak.bluetooth.Bluetooth;
+import me.aflak.bluetooth.interfaces.BluetoothCallback;
+import me.aflak.bluetooth.interfaces.DiscoveryCallback;
 
 public class BluetoothService extends IntentService {
     public BluetoothService(String name) {
         super(name);
+    }
+
+    Bluetooth bluetooth;
+
+    // Intoarce parola numai in cazul in care e autolock, altfel null
+    String getPassword(String name) {
+        SharedPreferences sharedpreferencesautlocks = getSharedPreferences("Autolog", Context.MODE_PRIVATE);
+        boolean isAutolock = sharedpreferencesautlocks.getBoolean(name, false);
+        if (isAutolock) {
+            SharedPreferences sharedpreferenceslocks = getSharedPreferences("Locks", Context.MODE_PRIVATE);
+            String password = sharedpreferenceslocks.getString(name, null);
+            return password;
+        }
+        return null;
     }
 
     // This is called on autolaunch
@@ -18,5 +41,17 @@ public class BluetoothService extends IntentService {
         String dataString = intent.getDataString();
         // Do work here, based on the contents of dataString
         // Sau doar codul pentru cautare continua si conectare
+        bluetooth = new Bluetooth(getApplicationContext());
+        bluetooth.setDiscoveryCallback(new DiscoveryCallback() {
+            @Override public void onDiscoveryStarted() {}
+            @Override public void onDiscoveryFinished() {}
+            @Override public void onDeviceFound(BluetoothDevice device) {
+                String name = device.getName();
+                String address = device.getAddress();
+            }
+            @Override public void onDevicePaired(BluetoothDevice device) {}
+            @Override public void onDeviceUnpaired(BluetoothDevice device) {}
+            @Override public void onError(int errorCode) {}
+        });
     }
 }
